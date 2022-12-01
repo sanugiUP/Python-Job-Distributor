a9aacb289bf6f06e3a632500c96a8721687d78b5
sanugiUP <113755041+sanugiUP@users.noreply.github.com>
2022-12-01 22:50:18 +0530

    Code For Website

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            RestClient restClient = new RestClient("http://localhost:55924/");
+            RestRequest request = new RestRequest("api/clients/getclients", Method.Get);
+            RestResponse response = restClient.Get(request);
+            List<Client> clients = JsonConvert.DeserializeObject<List<Client>>(response.Content);
+            return View(clients);
+        }
+
+    }
+}
---
+++ Website/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Website.Models
+{
+    public class Client
+    {
+        public string ipaddress { get; set; }
+        public int portnumber { get; set; }
+        public int jobscompleted { get; set; }
+    }
+}
